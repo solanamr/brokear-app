81c0f4ff8a58a5eb7be9c7b7f518b942c5c52cea
solanamr <romerosolana@gmail.com>
2023-07-19 10:35:35 -0300

    df

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Wave from "../../src/icons/Wave"
+import Wave from "../src/icons/Wave"
 
 export default function Home() {
   return (
     <main className="">
-      
+
     </main>
   )
 }
